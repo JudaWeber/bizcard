1907708bd18982a70b6890eeba0a02bc9def7f45
judaweb <107354039+judaweb@users.noreply.github.com>
2023-03-09 22:05:05 +0200

    fix helmet

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+const morgan = require("morgan");
 const debug = require("debug")("bizcard:app");
 const cors = require("cors");
 const helmet = require("helmet");
+const logger = require("./config/winston");
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 const apiRouter = require("./routes/api");
 
 const app = express();
 
 app.use(cors());
-app.use(helmet());
-app.use(logger("dev"));
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
+app.use(morgan("combined", { stream: logger.stream.write }));
+// app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, "public")));
 
-// app.use("/", indexRouter);
-// app.use("/users", usersRouter);
-
 app.use("/api", apiRouter);
-
+app.use("/api/*", (req, res) => {
+  throw new Error("error thrown navigate to");
+});
+app.use((err, req, res, next) => {
+  global.logger.error({
+    method: req.method,
+    error: err.message,
+    url: req.originalUrl,
+  });
+  next(err);
+});
 /* React*/
 app.use(express.static(path.join(__dirname, "build")));
 app.get("*", (req, res) => {
